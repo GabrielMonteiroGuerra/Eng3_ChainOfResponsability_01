03668f8e6fc4510186183f2e3ae382dfb8c3f0ed
dti <dti@LAB202-04>
2022-06-13 21:20:36 -0300

    Chain of Responsability - Completo

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -4,7 +4,17 @@ import model.Candidato;
 
 
 public class Principal {
-	Candidato c = new Candidato();
 	
 	
+	public static void main(String[] args) {
+		Candidato c = new Candidato();
+		
+		c.setId(1);
+		c.setAnosExperiencia(3);
+		c.setAnosRegistroEntidadeClasse(5);
+		c.setEnsinoMedioPublico(true);
+		c.setEnsinoSuperiorCompleto(true);
+	}
+
+
 }
